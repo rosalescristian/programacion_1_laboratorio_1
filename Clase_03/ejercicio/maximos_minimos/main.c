baf18518c5a338da4d30f56aa6d6eaf00957e031
rosalescristian <rosalescristianm@gmail.com>
2019-03-19 22:02:25 -0300

    Minimos y Maximos completos - Ademas agregue mis apuntes

--- Clase_03/ejercicio/maximos_minimos/main.c
+++ Clase_03/ejercicio/maximos_minimos/main.c
@@ -33,5 +33,8 @@ int main()
             rta = toupper(rta);// toupper = convierte el caracter en Mayusculas // to-Upper-Case // tolower -> todo a minusculas
             }while(rta != 'S' && rta != 'N');
     }while(rta == 'S');
+    system("cls");//limpiar la pantalla. Ejecuta desde la biblioteca que llamamos arriba.
+    printf("El nro maximo es %d\n El nro minimo es %d",max,min); // el \n va siempre en el primero xq es un salto de linea // Siempre tener en cuenta que el orden de los modificadores %d es el orden de las variables cmo se van a mostrar.
+
     return 0;
 }
