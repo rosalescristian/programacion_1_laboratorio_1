d72773acc47cbf88da7a8d33a49846757a42910a
rosalescristian <rosalescristianm@gmail.com>
2019-03-19 21:20:18 -0300

    SWITCH con caracteres

--- Clase_03/ejercicio/case/main.c
+++ Clase_03/ejercicio/case/main.c
@@ -9,7 +9,10 @@ int main()
     //De 6 a 9 "Levantate"
     //Default "Hola Mundo!"
 
+    //int hora; //funciona igual con tipo int o char.
+/*  //todo esto es con la variable hora como int - con char es distinto y es un bardo :P -- ver despues del comment
     int hora;
+    //char mensaje[255]; // no me salio sobreescribir el mensaje y mostrarlo una sola vez.
     printf("Ingrese una hora: ");
     scanf("%d",&hora);
         switch(hora)
@@ -21,15 +24,45 @@ int main()
             case 4:
             case 5:
                 printf("A dormir");
+                //  mensaje = "A dormir.";
                 break;
             case 6:
             case 7:
             case 8:
             case 9:
                 printf("Levantate");
+                //   mensaje = "Levantate";
+                break;
+            default:
+                printf("Hola Mundo!");
+                //mensaje = "Hola Mundo!";
+                //printf("mensaje %s\n",mensaje);
+        }
+    return 0;
+*/ // todo con caracteres de texto
+    char hora;
+    printf("Ingrese una hora: ");
+    fflush(stdin);
+    scanf("%c",&hora);
+        switch(hora)
+        {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+                printf("A dormir!");
+                break;
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                printf("Levantate!");
                 break;
             default:
                 printf("Hola Mundo!");
         }
-    return 0;
+        return 0;
+
 }
