0bbb5fbb4cbd26fbf773bfb5fa2adb1e25e862a7
alumno <alumno>
2019-04-08 20:15:12 -0300

    Mas comentarios y conceptos sobre contar y comparar caracteres en strings

--- Clase_06/ejercicio/Ejercicios con Strings/main.c
+++ Clase_06/ejercicio/Ejercicios con Strings/main.c
@@ -11,7 +11,6 @@
     - CUANDO SE TRABAJA CON VECTORES NO SE USA & EN LAS FUNCIONES
 */
 
-
 void main(void)
 {
     char nombre[55]; // hay que definir bien la cantidad de caracteres; como la memoria esta libre sigue tomando la info que le cargaron.
@@ -28,6 +27,12 @@ void main(void)
 
     Existe tb esta funcion fgets(); -> sirve para varias cosas y tb para controlar y no excederme en la cant de caracteres
     q tenemos declarada en la variable.
+    strlen(); -> cuenta la cantidad de caracteres y la guardo en una variable tipo int;
+    operador sizeof(); -> nos devuelve la cantidad de bytes q ocupa un tipo de dato o una variable en la memoria. Va el tipo de dato o la variable. (INT-CHAR-NOMBRE-EDAD-ETC.)
+
+    fgets(buffer,sizeof(buffer)-2,stdin); //FUNCIONA CON 3 ARGUMENTOS a)Variable Buffer, b)SizeOF es para limitar la cant de caracteres que voy a tomar, c)stdin (es para tomar el ingreso de teclado, Standard IN).
+
+    Del otro main, esta bueno para tomar esas 4 lineas de codigo que hacen toda la compilacion del string y usarla en una funcion propia
 */
 
     return 0;
--- Clase_06/ejercicio/fgets/main.c
+++ Clase_06/ejercicio/fgets/main.c
@@ -9,11 +9,11 @@ int main()
 
     printf("Nombre: ");
     //fgets(buffer, 62, stdin);
-    fgets(buffer,sizeof(buffer)-2,stdin);
+    fgets(buffer,sizeof(buffer)-2,stdin); // con el -2 nos aseguramos que no tome ningun caracter especial (fgets despues del ultimo caracter ingresa un caracter especial antes del cierre de linea \0. Y retorno dos posiciones.
 
-    cantidad = strlen(buffer);
+    cantidad = strlen(buffer); // cuento la cantidad de caracteres buffer y la guardo en cantidad como INT.
 
-    buffer[cantidad-1] = '\0';
+    buffer[cantidad-1] = '\0'; // JUAN*\0 es un array de 6 lugares, o sea 5 sin el cierre, le resto una posicion, y en ellugar del * escribo un \0.
 
     printf("%s", buffer);
 
